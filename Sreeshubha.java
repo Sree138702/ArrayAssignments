4ccd793f0885fe706a55d6a98b13b3b2ba3a5bee
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-07 22:37:17 +0100

    array assignments

---
+++ Sreeshubha.java
@@ -0,0 +1,19 @@
+package basicsofjava_Arrays;
+
+/*public class Upcasting_Name {
+	void multi() {
+		
+	}*/
+	public class Sreeshubha{
+		 void add() {
+			System.out.println("cute");
+		}
+	
+
+	public static void main(String[] args) {
+		
+     Sreeshubha s1= new Sreeshubha();
+     s1.add();
+	}
+
+}
